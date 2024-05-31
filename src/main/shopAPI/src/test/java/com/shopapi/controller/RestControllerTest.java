b15ea718d98f967ad2ac4ba2d75737b8561b9677
sparrvio <sparrvio@.ya.ru>
2024-05-31 19:54:45 +0300

    Tests getAllClientsIsTrueTest() without field birthday

--- src/main/shopAPI/src/test/java/com/shopapi/controller/RestControllerTest.java
+++ src/main/shopAPI/src/test/java/com/shopapi/controller/RestControllerTest.java
@@ -17,9 +17,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +31,9 @@ public class RestControllerTest {
     private MockMvc mockMvc;
 
     private ClientDTO clientDTO;
+    private ClientDTO clientDTO1;
+    private ClientDTO clientDTO2;
+    private List<ClientDTO> clientDTOList;
 
     @BeforeEach()
     void setUp() {
@@ -42,6 +45,24 @@ public class RestControllerTest {
                 .gender('M')
                 .registrationDate(LocalDate.of(2002, 2, 2))
                 .build();
+
+        clientDTO1 = ClientDTO.builder()
+                .clientName("Elena")
+                .clientSurname("Smirnova")
+                .birthday(LocalDate.of(1972, 1, 23))
+                .gender('F')
+                .registrationDate(LocalDate.of(2002, 2, 2))
+                .build();
+
+        clientDTO2 = ClientDTO.builder()
+                .clientName("Marina")
+                .clientSurname("Kozlova")
+                .birthday(LocalDate.of(1990, 10, 21))
+                .gender('F')
+                .registrationDate(LocalDate.of(2020, 12, 31))
+                .build();
+
+        clientDTOList = Arrays.asList(clientDTO, clientDTO1, clientDTO2);
     }
 
     @Test
@@ -54,6 +75,7 @@ public class RestControllerTest {
                 .andExpect(status().isOk());
 
         verify(clientService).getClientById(1L);
+        verify(clientService, times(1)).getClientById(1L);
     }
 
     @Test
@@ -65,5 +87,17 @@ public class RestControllerTest {
                 .andExpect(status().isNotFound());
 
         verify(clientService).getClientById(1L);
+        verify(clientService, times(1)).getClientById(1L);
+    }
+
+    @Test
+    void getAllClientsIsTrueTest() throws Exception  {
+        when(clientService.getAllClients()).thenReturn(clientDTOList);
+        mockMvc.perform(get("/api/v1/client/allClients"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].clientName").value("Ivan"))
+                .andExpect(jsonPath("$[1].clientName").value("Elena"))
+//                .andExpect(jsonPath("$[2].birthday").value(LocalDate.of(1990, 10, 21)))
+                .andExpect(jsonPath("$[1].gender").value("F"));
     }
 }
