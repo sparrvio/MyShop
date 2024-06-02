66676feaba6a51097e5f44a4d20689b367162e3c
sparrvio <sparrvio@.ya.ru>
2024-06-02 17:33:59 +0300

    Tests getClientByFullNameIsTrueTest() and getClientByFullNameIsFalseTest is  completed

--- src/main/shopAPI/src/test/java/com/shopapi/controller/RestControllerTest.java
+++ src/main/shopAPI/src/test/java/com/shopapi/controller/RestControllerTest.java
@@ -1,9 +1,11 @@
 package com.shopapi.controller;
 
+import com.shopapi.dto.AddressDTO;
 import com.shopapi.dto.ClientDTO;
 import com.shopapi.model.Client;
 import com.shopapi.service.ClientService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,11 +36,18 @@ public class RestControllerTest {
     private ClientDTO clientDTO;
     private ClientDTO clientDTO1;
     private ClientDTO clientDTO2;
+    private AddressDTO addressDTO;
     private List<ClientDTO> clientDTOList;
 
     @BeforeEach()
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(restController).build();
+        addressDTO  = AddressDTO.builder()
+                .country("Russia")
+                .city("Moscow")
+                .street("Tverskaya")
+                .build();
+
         clientDTO = ClientDTO.builder()
                 .clientName("Ivan")
                 .clientSurname("Petrov")
@@ -61,6 +71,8 @@ public class RestControllerTest {
                 .gender('F')
                 .registrationDate(LocalDate.of(2020, 12, 31))
                 .build();
+
+        clientDTO.setAddress(addressDTO);
 
         clientDTOList = Arrays.asList(clientDTO, clientDTO1, clientDTO2);
     }
@@ -97,7 +109,26 @@ public class RestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].clientName").value("Ivan"))
                 .andExpect(jsonPath("$[1].clientName").value("Elena"))
-//                .andExpect(jsonPath("$[2].birthday").value(LocalDate.of(1990, 10, 21)))
-                .andExpect(jsonPath("$[1].gender").value("F"));
+                .andExpect(jsonPath("$[2].gender").value("F"));
+    }
+
+    @Test
+    void getClientByFullNameIsTrueTest() throws Exception {
+        when(clientService.getClientByNameAndSurname("Ivan Petrov")).thenReturn(clientDTOList);
+        mockMvc.perform(get("/api/v1/client/search?fullName=Ivan Petrov"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].clientName").value("Ivan"))
+                .andExpect(jsonPath("$[0].clientSurname").value("Petrov"))
+                .andExpect(jsonPath("$[0].gender").value("M"))
+                .andExpect(jsonPath("$[0].address.country").value("Russia"))
+                .andExpect(jsonPath("$[0].address.city").value("Moscow"))
+                .andExpect(jsonPath("$[0].address.street").value("Tverskaya"));
+    }
+
+    @Test
+    void getClientByFullNameIsFalseTest() throws Exception  {
+        when(clientService.getClientByNameAndSurname("Name Surname")).thenReturn(Collections.emptyList());
+        mockMvc.perform(get("/api/v1/client/search?fullName=Name Surname"))
+                .andExpect(status().isOk());
     }
 }
