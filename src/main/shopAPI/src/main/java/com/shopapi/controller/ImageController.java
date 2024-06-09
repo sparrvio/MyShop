c9f5f96df1b706566d367050bd5cc224cbf9da54
sparrvio <sparrvio@.ya.ru>
2024-06-09 21:29:36 +0300

    ImageController getImageByIdImage() completed and getImageByIdProduct() in work

--- src/main/shopAPI/src/main/java/com/shopapi/controller/ImageController.java
+++ src/main/shopAPI/src/main/java/com/shopapi/controller/ImageController.java
@@ -2,25 +2,32 @@ package com.shopapi.controller;
 
 import com.shopapi.dto.ImagesDTO;
 import com.shopapi.dto.ProductDTO;
+import com.shopapi.mapper.ProductMapper;
 import com.shopapi.model.Images;
 import com.shopapi.model.Product;
 import com.shopapi.service.ImageService;
 import com.shopapi.service.ProductService;
-import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.SneakyThrows;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.Set;
 
 @OpenAPIDefinition(info = @Info(title = "Images API", version = "v1"))
 @RestController
@@ -30,6 +37,9 @@ public class ImageController {
     private ImageService imageService;
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private ProductMapper productMapper;
 
     @Operation(summary = "Create new image", description = "This endpoint allows creating a new image associated with a specific product.")
     @ApiResponses(value = {
@@ -42,6 +52,7 @@ public class ImageController {
                                             @RequestParam("imageFile") MultipartFile file) {
         String contentType = file.getContentType();
 
+        assert contentType != null;
         if (!contentType.startsWith("image/")) {
             return new ResponseEntity<>("Invalid image type", HttpStatus.BAD_REQUEST);
         }
@@ -57,22 +68,73 @@ public class ImageController {
         return new ResponseEntity<>("Image created successfully", HttpStatus.OK);
     }
 
-    @Operation(summary = "Get images by ID")
+    @Operation(summary = "Get picture by ID image")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation"),
             @ApiResponse(responseCode = "400", description = "Bad request - invalid page or size parameters"),
             @ApiResponse(responseCode = "404", description = "Image not found")
     })
     @GetMapping("/image/ImageId")
-    public ResponseEntity<?> getImageByIdImage(@ApiParam(value = "Image ID") @RequestParam Long ImageId) {
+    public ResponseEntity<?> getImageByIdImage(@RequestParam Long ImageId) {
         if (ImageId == null || ImageId < 1) {
-            return new ResponseEntity<>("Invalid client ID", HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().body(null);
         }
-        Optional <ImagesDTO> imagesDTOOptional = imageService.getImages(ImageId);
-        if(imagesDTOOptional.isEmpty())  {
+        Optional<ImagesDTO> imagesDTOOptional = imageService.getImages(ImageId);
+        if (imagesDTOOptional.isEmpty()) {
             return new ResponseEntity<>("Image not found", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(imagesDTOOptional.get(), HttpStatus.OK);
+
+        byte[] imageBytes = imagesDTOOptional.get().getImageBytes();
+
+        try {
+            ImageIO.read(new ByteArrayInputStream(imageBytes));
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+            ByteArrayResource resource = new ByteArrayResource(imageBytes, headers.toString());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+    @Operation(summary = "Get picture by ID product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request - invalid page or size parameters"),
+            @ApiResponse(responseCode = "404", description = "Image not found")
+    })
+    @GetMapping("/image/productID")
+    public ResponseEntity<?> getImageByIdProduct(@RequestParam Long productID) {
+        if (productID == null || productID < 1) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        Optional<ProductDTO> productDTOOptional = productService.getById(productID);
+        if (productDTOOptional.isEmpty()) {
+            System.out.println("if (productDTOOptional.isEmpty())");
+            return ResponseEntity.notFound().build();
+        } else {
+            ProductDTO productDTO = productDTOOptional.get();
+            Product product = productMapper.toEntity(productDTO);
+            Set<Images> images  = product.getImages();
+            return new ResponseEntity<>(images, HttpStatus.OK);
+        }
+    }
+
+    @Operation(summary  =  "Delete image by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Image deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Image not found")
+    })
+    @GetMapping(value = "/image/delete")
+   public ResponseEntity<?> delete(@RequestParam @Valid Long imageIDForDelete) {
+        Optional<ImagesDTO> imagesDTOOptional  = imageService.getImages(imageIDForDelete);
+        if  (imagesDTOOptional.isEmpty())  {
+            return new ResponseEntity<>("Image not found", HttpStatus.NOT_FOUND);
+        }
+        imageService.deleteImage(imageIDForDelete);
+        return new ResponseEntity<>("Image deleted successfully", HttpStatus.OK);
+    }
 }
