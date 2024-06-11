3aac2231370b5ec7fe32b9950060d7d61e516de6
sparrvio <sparrvio@.ya.ru>
2024-06-11 22:22:41 +0300

    refactor: add field for SupplierMapper

--- src/main/shopAPI/src/main/java/com/shopapi/mapper/SupplierMapper.java
+++ src/main/shopAPI/src/main/java/com/shopapi/mapper/SupplierMapper.java
@@ -13,13 +13,15 @@ import java.util.Set;
 @Mapper(componentModel = "spring", uses = {AddressMapper.class, ProductMapper.class})
 public interface SupplierMapper {
     @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target  =  "supplierName")
-    @Mapping(source  =  "address_id", target  =  "supplierAddress")
-    @Mapping(source  =  "phone_number", target  =  "supplierPhone")
+    @Mapping(source = "name", target = "supplierName")
+    @Mapping(source = "address_id", target = "supplierAddress")
+    @Mapping(source = "phone_number", target = "supplierPhone")
+    @Mapping(source = "products", target = "products")
     SupplierDTO toDTO(Supplier supplier);
+
     @Mapping(source = "id", target = "id")
-    @Mapping(source = "supplierName", target  =  "name")
-    @Mapping(source  =  "supplierAddress", target  =  "address_id")
-    @Mapping(source  =  "supplierPhone", target  =  "phone_number")
+    @Mapping(source = "supplierName", target = "name")
+    @Mapping(source = "supplierAddress", target = "address_id")
+    @Mapping(source = "supplierPhone", target = "phone_number")
     Supplier toSupplier(SupplierDTO supplierDTO);
 }
