09e96d36d3c094cf2b343998a92ae2b5419eb3e7
sparrvio <sparrvio@.ya.ru>
2024-06-22 21:43:38 +0300

    fix: delete annotations @Schema

--- src/main/shopAPI/src/main/java/com/shopapi/dto/SupplierDTO.java
+++ src/main/shopAPI/src/main/java/com/shopapi/dto/SupplierDTO.java
@@ -10,17 +10,12 @@ import java.util.Set;
 @ApiModel(description = "Supplier details")
 @Data
 @Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
 public class SupplierDTO {
-    @Schema(description = "Unique identifier of the supplier", example = "1")
     private Long id;
-    @Schema(description =  "Name of the client", example  = "Apple")
+    @Schema(description =  "Name of the supplier", example  = "Apple")
     private String supplierName;
-    @Schema(description  =  "address of the supplier", example  =  "address")
     private AddressDTO supplierAddress;
     @Schema(description  =  "phone of the supplier", example  =  "+7-999-999-99-99")
     private String supplierPhone;
-    @Schema(description  =  "products of the supplier", example  ="product")
     private Set<ProductDTO> products;
 }
